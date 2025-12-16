de8978af47b23e067c2bb343fa66fb1e3f785e01
giuliaborra <giulia.borra2001@gmail.com>
2025-12-16 17:50:59 +0100

    SPERIAMO NON SI ROMPA

--- Assets/Animations/Animation Script/FridgeRightDoorController.cs
+++ Assets/Animations/Animation Script/FridgeRightDoorController.cs
@@ -1,30 +1,21 @@
 using UnityEngine;
 
-<<<<<<< HEAD
 public enum DoorSide { Left, Right }
 
-=======
->>>>>>> feature/player-wasd-teleport
 [RequireComponent(typeof(Collider))]
 public class FridgeDoorController : MonoBehaviour
 {
     [SerializeField] private Animator animator;
 
-<<<<<<< HEAD
     // Indica se questa porta � la destra o la sinistra
     [SerializeField] private DoorSide side;
 
     private bool isOpen;
-=======
-    private bool isOpen;
-    private int openHash;
->>>>>>> feature/player-wasd-teleport
     private Collider col;
 
     void Awake()
     {
         col = GetComponent<Collider>();
-<<<<<<< HEAD
 
         // Porta inizialmente chiusa
         isOpen = false;
@@ -37,30 +28,11 @@ public class FridgeDoorController : MonoBehaviour
             animator.Play("FridgeLeftDoorClosed", 0, 1f);
 
         animator.Update(0f);
-=======
-        
-        openHash = Animator.StringToHash("Open");
-
-        // Porta inizialmente chiusa
-            isOpen = false;
-            animator.SetBool(openHash, isOpen);
-
-        // Forza un'update immediato per applicare il parametro all'Animator al caricamento
-        // (utile se l'Animator usa PlayOnAwake o ha transizioni immediate)
-            animator.Update(0f);
-        
->>>>>>> feature/player-wasd-teleport
     }
 
     void OnMouseDown()
     {
         isOpen = !isOpen;
-<<<<<<< HEAD
         animator.SetBool("Open", isOpen);
     }
 }
-=======
-        animator.SetBool(openHash, isOpen);
-    }
-}
->>>>>>> feature/player-wasd-teleport
